8d88648cc348ad343682b51b6ea26b27ba8b79d5
Tako-San <53308786+Tako-San@users.noreply.github.com>
2026-02-11 15:29:15 +0300

    Create ComponentDebugIntrinsics.scala

--- plugin/src/main/scala-2/chisel3/internal/plugin/ComponentDebugIntrinsics.scala
+++ plugin/src/main/scala-2/chisel3/internal/plugin/ComponentDebugIntrinsics.scala
@@ -6,38 +6,18 @@ import scala.tools.nsc
 import scala.tools.nsc.{Global, Phase}
 import scala.tools.nsc.plugins.PluginComponent
 
-/** Compiler plugin injecting debug intrinsics for Chisel signals.
-  *
-  * Generates debug metadata for Wire, Reg, IO, and other Chisel Data types.
-  * Activated via compiler option: -P:chiselplugin:addDebugIntrinsics
-  */
-class ComponentDebugIntrinsics(
-  val global: Global,
-  arguments: ChiselPluginArguments
-) extends PluginComponent {
+class ComponentDebugIntrinsics(val global: Global, arguments: ChiselPluginArguments) extends PluginComponent {
   import global._
-
   val runsAfter: List[String] = List("typer")
   val phaseName: String = "componentDebugIntrinsics"
+  override def description: String = "Debug Intrinsics (Skeleton)"
 
-  override def description: String = "Inject debug metadata for Chisel Data signals"
-
-  def newPhase(prev: Phase): ComponentDebugIntrinsicsPhase =
-    new ComponentDebugIntrinsicsPhase(prev)
-
-  class ComponentDebugIntrinsicsPhase(prev: Phase) extends StdPhase(prev) {
+  def newPhase(prev: Phase): Phase = new StdPhase(prev) {
     override def name: String = phaseName
-
     def apply(unit: CompilationUnit): Unit = {
-      if (ChiselPlugin.runComponent(global, arguments)(unit) && arguments.addDebugIntrinsics) {
-        // Temporary logging to verify plugin activation in tests
-        println(s"[CHISEL-DEBUG-INTRINSICS] Phase running on: ${unit.source.file.name}")
-
-        // TODO: Implement actual transformation logic:
-        // 1. Traverse AST for Wire/Reg/IO calls
-        // 2. Extract source locations and type information
-        // 3. Inject chisel3.reflect.DataMirror.addDebugInfo() calls
-      }
+        if (arguments.addDebugIntrinsics) {
+            global.reporter.warning(unit.body.pos, "Debug Intrinsics Phase Running")
+        }
     }
   }
 }
