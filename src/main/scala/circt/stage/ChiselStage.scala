1487c9bb0c43c3018a6033e28de330b2a859f9f3
Farid Khaydari <f.khaydari@syntacore.com>
2026-02-19 17:11:23 +0300

    Remove CollectDebugInfo from ChiselStage targets

--- src/main/scala/circt/stage/ChiselStage.scala
+++ src/main/scala/circt/stage/ChiselStage.scala
@@ -33,7 +33,6 @@ class ChiselStage extends Stage {
         Dependency[chisel3.stage.phases.AddImplicitOutputFile],
         Dependency[chisel3.stage.phases.AddImplicitOutputAnnotationFile],
         Dependency[chisel3.stage.phases.AddSerializationAnnotations],
-        Dependency[chisel3.stage.phases.CollectDebugInfo],
         Dependency[chisel3.stage.phases.Convert],
         Dependency[chisel3.stage.phases.AddDedupGroupAnnotations],
         Dependency[circt.stage.phases.AddImplicitOutputFile],
